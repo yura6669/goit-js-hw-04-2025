6c2788c4d3c99e37a9854c456ecdf3c0544c6c05
yura6669 <yurii.shyian.01@gmail.com>
2025-04-11 22:10:50 +0300

    Implement tasks

---
+++ js/task-1.js
@@ -0,0 +1,26 @@
+console.log('------------------- Task 1 -------------------');
+
+function isEnoughCapacity(products, containerSize) {
+    const valuse = Object.values(products);
+    let total = 0;
+    for (let i = 0; i < valuse.length; i++) { 
+        total += valuse[i];
+    }
+    return total <= containerSize;
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
